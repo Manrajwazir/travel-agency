4feda1db154fd628e6440ba39a9a3a1597a0e618
Manrajwazir <manrajwazir@gmail.com>
2025-08-20 17:27:53 -0600

    routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route, layout } from "@react-router/dev/routes";
+export default [
+    layout("routes/admin/admin-layout.tsx",[
+        route('dashboard', 'routes/admin/dashboard.tsx'),
+        route('all-users', 'routes/admin/all-users.tsx')
+    ]),
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+] satisfies RouteConfig;
---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AllUsers = () => {
+    return (
+        <div>Users</div>
+    )
+}
+export default AllUsers
